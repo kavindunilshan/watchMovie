7c89f41b9b41e142a72c325634ca6bb4ef31089d
Kavindu Nilshan <kavindunilshanliyanage@gmail.com>
2023-08-01 19:34:26 +0530

    User Created

---
+++ src/main/java/com/example/watchMovie/entity/Theater.java
@@ -0,0 +1,53 @@
+package com.example.watchMovie.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Theater {
+    @Id
+    @Column
+    private int tid;
+
+    @Column
+    private String name;
+
+    @Column
+    private int contactNumber;
+
+    public int getTid() {
+        return tid;
+    }
+
+    public void setTid(int tid) {
+        this.tid = tid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(int contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Theater{" +
+                "tid=" + tid +
+                ", name='" + name + '\'' +
+                ", contactNumber=" + contactNumber +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/watchMovie/service/TheaterService.java
@@ -0,0 +1,50 @@
+package com.example.watchMovie.service;
+
+import com.example.watchMovie.entity.Theater;
+import com.example.watchMovie.repository.TheaterRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TheaterService {
+    @Autowired
+    private TheaterRepository theaterRepository;
+
+    public Theater createTheater(Theater theater) {
+        return theaterRepository.save(theater);
+    }
+
+    public List<Theater> createTheaterList(List<Theater> list) {
+        return theaterRepository.saveAll(list);
+    }
+
+    public List<Theater> getTheaterList() {
+        return theaterRepository.findAll();
+    }
+
+    public Theater getTheaterById(int tid) {
+        return theaterRepository.findById(tid).orElse(null);
+    }
+
+    public Theater updateTheaterById(Theater theater) {
+        Optional<Theater> theaterFound = theaterRepository.findById(theater.getTid());
+        if (theaterFound.isPresent()) {
+            Theater theaterUpdate = theaterFound.get();
+            theaterUpdate.setName(theater.getName());
+            theaterUpdate.setContactNumber(theater.getContactNumber());
+
+            return theaterRepository.save(theater);
+        } else {
+            return null;
+        }
+    }
+
+    public String deleteById(int tid) {
+        theaterRepository.deleteById(tid);
+        return "Theater " + tid + " is deleted.";
+    }
+
+}
