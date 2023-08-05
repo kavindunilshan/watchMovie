7ce87a1bbe7ec65e70b7fbbc6368d8078db42525
Kavindu Nilshan <kavindunilshanliyanage@gmail.com>
2023-08-05 18:37:50 +0530

    Add custom Exception class.

---
+++ src/main/java/com/example/watchMovie/exception/IdNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.watchMovie.exception;
+
+public class IdNotFoundException extends Exception {
+    public IdNotFoundException() {
+        super();
+    }
+
+    public IdNotFoundException(String message) {
+        super(message);
+    }
+}
