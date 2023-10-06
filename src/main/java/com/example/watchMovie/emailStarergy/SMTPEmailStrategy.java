61ae2277e6dfea442309f80bd50d4380ae42eeef
Kavindu Nilshan <kavindunilshanliyanage@gmail.com>
2023-10-06 16:40:50 +0530

    Fixed the password leakage issue.

--- src/main/java/com/example/watchMovie/emailStarergy/SMTPEmailStrategy.java
+++ src/main/java/com/example/watchMovie/emailStarergy/SMTPEmailStrategy.java
@@ -1,5 +1,6 @@
 package com.example.watchMovie.emailStarergy;
 import com.example.watchMovie.resources.PdfCreator;
+import org.springframework.beans.factory.annotation.Value;
 
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
@@ -12,6 +13,13 @@ import javax.mail.internet.MimeMultipart;
 import java.util.Properties;
 
 public class SMTPEmailStrategy extends EmailSenderStrategy {
+
+    @Value("${spring.mail.username}")
+    private static String username;
+
+    @Value("${spring.mail.password}")
+    private static String password;
+
     public SMTPEmailStrategy() {
     }
 
@@ -22,8 +30,8 @@ public class SMTPEmailStrategy extends EmailSenderStrategy {
     @Override
     public void sendEmail() {
 
-        final String username = "kavinilj10@gmail.com";
-        final String password = "qfshespookvyxpor";
+//        final String username = "kavinilj10@gmail.com";
+//        final String password = "qfshespookvyxpor";
 
         Properties prop = new Properties();
 
